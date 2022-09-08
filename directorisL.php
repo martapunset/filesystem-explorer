78293f5bc4a17e7bb24a6526060e883b9f6ec1fc
Marta Punset <martapunset10@gmail.com>
2022-09-08 11:09:11 +0200

    path changed

---
+++ directorisL.php
@@ -0,0 +1,13 @@
+<?php
+
+$path = "ROOT";
+if ($fh = opendir($path)){
+    while(($entry = readdir($fh)) !== false) {
+        if ($entry != "." && $entry != ".."){
+            ?>
+            <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded"><?php echo $entry?></a></li>
+           <?php
+        }
+    }
+}
+?>
