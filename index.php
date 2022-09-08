50c9800abf3dcb4730650cc4f917bcd807e3f28c
zzezequiel <ezequielzvirgzdins@gmail.com>
2022-09-08 14:58:17 +0200

    dashboard

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
   </head>
   <body>
     
-<main class="d-flex flex-nowrap">
+<main class="d-flex align-items-stretch w-100 p-3">
   
   <div class="flex-shrink-0 p-3 bg-white" style="width: 280px;">
     <a href="" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
@@ -37,24 +37,35 @@
   </div>
 
   <!-- dashboard-->
-  <div class="my-3 p-3 bg-body rounded shadow-sm container-xl">
-    <div><h6 class="border-bottom pb-2 mb-0">Suggestions</h6></div>
+
+  <div class=" my-3 mh-100 bg-body rounded shadow-sm container-xl">
+    <div class="border-bottom pb-2 mb-0 d-flex justify-content-between">
+      <h6>File</h6>
+      <h6>Size</h6>
+      <h6>Modified</h6>
+  </div>
     
     <div class="d-flex text-muted pt-3">
       <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
 
-      <div class="pb-3 mb-0 small lh-sm border-bottom w-100">
+      <div class=" mb-0  border-bottom w-100">
         <div class="d-flex justify-content-between">
-          <strong class="text-gray-dark">Full Name</strong>
-          <a href="#">Follow</a>
+          <strong class="text-gray-dark">File name</strong>
+          <p>file size</p>
+          <p>file Modified</p>
         </div>
-        </div>
+      </div>
+    </div>
   </div>
-  </div>
-  </div>
+
+
   
 </main>
-
+<!--<footer class="footer mt-auto py-3 bg-light ">
+  <div class="container">
+    <span class="text-muted">footer</span>
+  </div>
+</footer>-->
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
