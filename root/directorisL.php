395b699bb5f4e98303744176814746a75cf4f46e
zzezequiel <ezequielzvirgzdins@gmail.com>
2022-09-08 10:35:26 +0200

    clean

--- root/directorisL.php
+++ root/directorisL.php
@@ -5,8 +5,7 @@ if ($fh = opendir($path)){
     while(($entry = readdir($fh)) !== false) {
         if ($entry != "." && $entry != ".."){
             ?>
-            <div>
-                <button class='btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed' data-bs-toggle='collapse' data-bs-target='#home-collapse' aria-expanded='false'><?php echo $entry?></button></div>
+            <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded"><?php echo $entry?></a></li>
            <?php
         }
     }
