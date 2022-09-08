473fc20ff78513172fa122f8b3300e5a50342b4e
Marta Punset <martapunset10@gmail.com>
2022-09-08 10:42:01 +0200

    g

--- index.php
+++ index.php
@@ -5,25 +5,91 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+
+
 </head>
 <body>
+   
+
+
+
+<main class="d-flex flex-nowrap">
+  
+  <div class="flex-shrink-0 p-3 bg-white" style="width: 280px;">
+    <a href="" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
+      
+      <span class="fs-5 fw-semibold">FILE SYSTEM EXPLORER</span>
+    </a>
+    <ul class="list-unstyled ps-0">
+      <li class="mb-1">
+      <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
+          Documents
+        </button>
+       
+        <div class="collapse show" id="home-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <?php include("openFolders.php");?>
+           
+            
+          </ul>
+        </div>
+      </li>
+      <li class="mb-1">
+        <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
+          Dashboard
+        </button>
+        <div class="collapse" id="dashboard-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Overview</a></li>
+            <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Weekly</a></li>
+            <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Monthly</a></li>
+            <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Annually</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="mb-1">
+        <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
+          Orders
+        </button>
+        <div class="collapse" id="orders-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">New</a></li>
+            <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Processed</a></li>
+            <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Shipped</a></li>
+            <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Returned</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="border-top my-3"></li>
+      <li class="mb-1">
+        <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
+          Account
+        </button>
+        <div class="collapse" id="account-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">New...</a></li>
+            <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Profile</a></li>
+            <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Settings</a></li>
+            <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Sign out</a></li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+  </div>
+
+  
+</main>
+
+
 
 <?php
-$dir="./ROOT";
-//$dir = "/images/";
 
-// Open a directory, and read its contents
-if (is_dir($dir)){
-  if ($dha = opendir($dir)){
-    while (($folder = readdir($dha)) !== false){
-     //  if(is_dir($folder)){ //si no es un file
-        var_dump(is_dir($folder));
-        echo $folder . "<br>";
-     //  }
-    }
-    closedir($dha);
-  }
-}
+
+
+
 
 
 
@@ -60,4 +126,5 @@ function listar_directorios_ruta($ruta){
 ?>
     
 </body>
+
 </html>
---
+++ openFolders.php
@@ -0,0 +1,23 @@
+<?php
+$dir="./ROOT";
+//$dir = "/images/";
+
+// Open a directory, and read its contents
+if (is_dir($dir)){
+  if ($dha = opendir($dir)){
+    while (($folder = readdir($dha)) !== false){
+     //  if(is_dir($folder)){ //si no es un file
+        if($folder!="." AND $folder !="..")
+        echo "<li><a href='#' class='link-dark d-inline-flex text-decoration-none rounded'>$folder </a></li>";
+     //  }
+    }
+    closedir($dha);
+  }
+}
+
+
+
+
+
+
+// <?php include("openFolders.php");?>
