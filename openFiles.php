935416002857e1f51f343862205b00e50c365929
Marta Punset <martapunset10@gmail.com>
2022-09-09 16:27:01 +0200

    dashbord fixed

--- openFiles.php
+++ openFiles.php
@@ -13,7 +13,8 @@ if(isset($_GET['name']) ){
           <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
           <div class=" container  mb-0  border-bottom w-100">
             <div class="row">
-              <strong class="text-gray-dark col"><?php echo $entry; ?></strong>
+            
+              <a class="text-gray-dark col" href="index.php?name=<?php echo $thisEntry?>"><strong> <?php echo $entry ?></strong></a>
               <p class="col text-center"><?php echo filesize($thisEntry)/1000 . "KB" ?></p>
               <p class="col text-end"><?php echo date("F d Y H:i:s", 
                               filemtime($thisEntry)); ?></p>
