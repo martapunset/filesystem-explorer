793ed49fd1bb50ad5f045024ec740f536a2f685f
Marta Punset <martapunset10@gmail.com>
2022-09-12 15:57:35 +0200

    code comments

--- openFolder.php
+++ openFolder.php
@@ -4,35 +4,33 @@ require_once("index.php");
 require_once("updatesession.php");
 
 
-if(isset($_GET['name']) ){
-
-    $url=$_GET['name'];  
-    
-    //no esta actualizando correctamente
+if(isset($_GET['name']) ){ 
+   $url=$_GET['name'];  
+       
 }else{
 
   $url="ROOT";
 }
+//configurate actual url  in session
 $_SESSION["newsession"]=$url;
-    actualSession($_SESSION["newsession"]);
+//call actual session function 
+//check if it doesnt need the atualsession function and works only with session calll
+actualSession($_SESSION["newsession"]);
 
+
+    //function to open the folders
 function openFolder (){
-$url=$_SESSION["newsession"];
+   $url=$_SESSION["newsession"];
 
     if(is_dir($url)){
-
-      
         if ($fh = opendir($url) ){
            while(($entry2 = readdir($fh)) !== false) {
               $thisEntry2=$url . DIRECTORY_SEPARATOR . $entry2;
                if ($entry2 != "." && $entry2 != ".."){
               
                 
-     
-                
-                
                 ?>      
-    <div class="d-flex text-muted pt-3">
+       <div class="d-flex text-muted pt-3">
           <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
           <div class=" container  mb-0  border-bottom w-100">
             <div class="row">
