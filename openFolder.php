ddf182b4b253789fb783010d02430bcdf63ca985
Marta Punset <martapunset10@gmail.com>
2022-09-12 15:49:44 +0200

    url new folder and actual url bar fixed

--- openFolder.php
+++ openFolder.php
@@ -1,34 +1,65 @@
 <?php
-require_once("index.php");
 
-function openFolder (){
+require_once("index.php");
+require_once("updatesession.php");
 
 
 if(isset($_GET['name']) ){
 
     $url=$_GET['name'];  
+    
+    //no esta actualizando correctamente
+}else{
+
+  $url="ROOT";
+}
+$_SESSION["newsession"]=$url;
+    actualSession($_SESSION["newsession"]);
+
+function openFolder (){
+$url=$_SESSION["newsession"];
+
     if(is_dir($url)){
+
+      
         if ($fh = opendir($url) ){
-           while(($entry = readdir($fh)) !== false) {
-              $thisEntry=$url . DIRECTORY_SEPARATOR . $entry;
-               if ($entry != "." && $entry != ".."){
+           while(($entry2 = readdir($fh)) !== false) {
+              $thisEntry2=$url . DIRECTORY_SEPARATOR . $entry2;
+               if ($entry2 != "." && $entry2 != ".."){
+              
+                
+     
+                
+                
                 ?>      
     <div class="d-flex text-muted pt-3">
           <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
           <div class=" container  mb-0  border-bottom w-100">
             <div class="row">
             
-              <a class="text-gray-dark col" href="index.php?name=<?php echo $thisEntry?>"><strong> <?php echo $entry ?></strong></a>
-              <p class="col text-center"><?php echo filesize($thisEntry)/1000 . "KB" ?></p>
+              <a class="text-gray-dark col" href="index.php?name=<?php echo $thisEntry2?>"><strong> <?php echo $entry2 ?></strong></a>
+              <p class="col text-center"><?php echo filesize($thisEntry2)/1000 . "KB" ?></p>
               <p class="col text-end"><?php echo date("F d Y H:i:s", 
-                              filemtime($thisEntry)); ?></p>
+                              filemtime($thisEntry2));
+                              
+     ?></p>
             </div>
           </div>
         </div>           
                <?php    
+              
             } 
+            
         }
+
+        //store session in external function
+     //   updateSession($_SESSION["newsession"]); 
+
     }
+
+  
+          
+   
 }else{
 
 //read files
@@ -41,7 +72,7 @@ echo "alosfdlkansf";
 
 
 
-}
+
 
 
 
